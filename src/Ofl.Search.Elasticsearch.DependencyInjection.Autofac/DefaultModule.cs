db4d11534a7ef391282714a5603734d648d1d6ab
casperOne <casperOne@caspershouse.com>
2017-04-18 16:38:50 -0400

    Initial migration.

---
+++ src/Ofl.Search.Elasticsearch.DependencyInjection.Autofac/DefaultModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System;
+
+namespace Ofl.Search.Elasticsearch.DependencyInjection.Autofac
+{
+    public class DefaultModule : Module
+    {
+        #region Overrides of Module
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Validate parameters.
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            // Bind.
+            builder.RegisterType<ElasticClientFactory>().As<IElasticClientFactory>();
+        }
+
+        #endregion
+    }
+}
---
+++ test/Ofl.Search.Elasticsearch.DependencyInjection.Autofac.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Ofl.Search.Elasticsearch.DependencyInjection.Autofac.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
